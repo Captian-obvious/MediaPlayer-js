7d49a320ec181022f59a05bdcbe92de6d3181aa2
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-05-11 07:58:06 -0400

    Update functions.js

--- dist/src/functions.js
+++ dist/src/functions.js
@@ -3,4 +3,21 @@ window.functions = {
         var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" + paramText;
         window.history.pushState({ path: newurl }, "", newurl);
     },
+    getRMS: function(arr) {
+        var square = 0;
+        var mean = 0;
+        var val = 0;
+        var rms = 0;
+        var n = arr.length;
+        // Calculate square.
+        for (var i = 0; i < n; i++) {
+            square += Math.pow(arr[i], 2);
+        };
+        // Calculate Mean.
+        mean = square / n;
+        // Calculate Root.
+        val = Math.sqrt(mean);
+        rms = val
+        return rms;
+    },
 }
