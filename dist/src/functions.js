b6bda1841ddcb428329db2ed48b36460638cb7f4
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-05-11 07:57:14 -0400

    Create functions.js

---
+++ dist/src/functions.js
@@ -0,0 +1,6 @@
+window.functions = {
+    replaceurl: function (paramText) {
+        var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" + paramText;
+        window.history.pushState({ path: newurl }, "", newurl);
+    },
+}
